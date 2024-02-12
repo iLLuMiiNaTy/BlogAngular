aeec7ebb1cd5b2bc288120839ab9bbb4b01a8a3e
ananunez <ananunezrejon@gmail.com>
2024-02-12 20:04:17 +0100

    segundo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BlogComponent } from './components/blog/blog.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, AppComponent, BlogComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'blog-SPA';
+}
+export class AppModule { }
